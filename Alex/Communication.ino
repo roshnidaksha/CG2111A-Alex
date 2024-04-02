af1b46019353fcddcab35056eb399d3d5364fe22
roshnidaksha <141601082+roshnidaksha@users.noreply.github.com>
2024-04-02 16:54:23 +0800

    Create Communication.ino

---
+++ Alex/Communication.ino
@@ -0,0 +1,43 @@
+/*
+ * Setup and start codes for serial communications
+ * 
+ */
+// Set up the serial connection. For now we are using 
+// Arduino Wiring, you will replace this later
+// with bare-metal code.
+void setupSerial() {
+  // To replace later with bare-metal.
+  Serial.begin(9600);
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using the other UARTs
+}
+
+// Start the serial connection. For now we are using
+// Arduino wiring and this function is empty. We will
+// replace this later with bare-metal code.
+
+void startSerial() {
+  // Empty for now. To be replaced with bare-metal code
+  // later on.
+}
+
+// Read the serial port. Returns the read character in
+// ch if available. Also returns TRUE if ch is valid. 
+// This will be replaced later with bare-metal code.
+
+int readSerial(char *buffer) {
+  int count = 0;
+
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+  while(Serial.available()) {
+    buffer[count++] = Serial.read();
+  }
+  return count;
+}
+
+// Write to the serial port. Replaced later with
+// bare-metal code
+
+void writeSerial(const char *buffer, int len) {
+  Serial.write(buffer, len);
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+}
