20ec8205f71a900a8b1e3813a796e5d3528e610a
roshnidaksha <141601082+roshnidaksha@users.noreply.github.com>
2024-04-02 16:50:50 +0800

    Create Routines.ino

---
+++ Alex/Routines.ino
@@ -0,0 +1,127 @@
+/*
+ * 
+ * Alex Communication Routines.
+ * 
+ */
+ 
+TResult readPacket(TPacket *packet) {
+  // Reads in data from the serial port and
+  // deserializes it.Returns deserialized
+  // data in "packet".
+    
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = readSerial(buffer);
+
+  if(len == 0) {
+    return PACKET_INCOMPLETE;
+  } else {
+    return deserialize(buffer, len, packet);
+  }    
+}
+
+void sendStatus() {
+  // Implement code to send back a packet containing key
+  // information like leftTicks, rightTicks, leftRevs, rightRevs
+  // forwardDist and reverseDist
+  // Use the params array to store this information, and set the
+  // packetType and command files accordingly, then use sendResponse
+  // to send out the packet. See sendMessage on how to use sendResponse.
+
+  TPacket statusPacket;
+  statusPacket.packetType = PACKET_TYPE_RESPONSE;
+  statusPacket.command = RESP_STATUS;
+  
+  statusPacket.params[0] = leftForwardTicks;
+  statusPacket.params[1] = rightForwardTicks;
+  statusPacket.params[2] = leftReverseTicks;
+  statusPacket.params[3] = rightReverseTicks;
+  
+  statusPacket.params[4] = leftForwardTicksTurns;
+  statusPacket.params[5] = rightForwardTicksTurns;
+  statusPacket.params[6] = leftReverseTicksTurns;
+  statusPacket.params[7] = rightReverseTicksTurns;
+  
+  statusPacket.params[8] = forwardDist;
+  statusPacket.params[9] = reverseDist;
+
+  sendResponse(&statusPacket);
+}
+
+void sendMessage(const char *message) {
+  // Sends text messages back to the Pi. Useful
+  // for debugging.
+  
+  TPacket messagePacket;
+  messagePacket.packetType = PACKET_TYPE_MESSAGE;
+  strncpy(messagePacket.data, message, MAX_STR_LEN);
+  sendResponse(&messagePacket);
+}
+
+void dbprintf(char *format, ...)  { 
+  // dbprintf("PI is %3.2f", PI) 
+  // This will make whatever you print appear on the Alex client program running on the Pi
+  // If what is being printed exceeds 128 bytes in total, Arduino will crash
+
+  va_list args; 
+  char buffer[128]; 
+
+  va_start(args, format); 
+  vsprintf(buffer, format, args); 
+  sendMessage(buffer);  
+} 
+
+void sendBadPacket() {
+  // Tell the Pi that it sent us a packet with a bad
+  // magic number.
+  
+  TPacket badPacket;
+  badPacket.packetType = PACKET_TYPE_ERROR;
+  badPacket.command = RESP_BAD_PACKET;
+  sendResponse(&badPacket);
+}
+
+void sendBadChecksum() {
+  // Tell the Pi that it sent us a packet with a bad
+  // checksum.
+  
+  TPacket badChecksum;
+  badChecksum.packetType = PACKET_TYPE_ERROR;
+  badChecksum.command = RESP_BAD_CHECKSUM;
+  sendResponse(&badChecksum);  
+}
+
+void sendBadCommand() {
+  // Tell the Pi that we don't understand its
+  // command sent to us.
+  
+  TPacket badCommand;
+  badCommand.packetType=PACKET_TYPE_ERROR;
+  badCommand.command=RESP_BAD_COMMAND;
+  sendResponse(&badCommand);
+}
+
+void sendBadResponse() {
+  TPacket badResponse;
+  badResponse.packetType = PACKET_TYPE_ERROR;
+  badResponse.command = RESP_BAD_RESPONSE;
+  sendResponse(&badResponse);
+}
+
+void sendOK() {
+  TPacket okPacket;
+  okPacket.packetType = PACKET_TYPE_RESPONSE;
+  okPacket.command = RESP_OK;
+  sendResponse(&okPacket);  
+}
+
+void sendResponse(TPacket *packet) {
+  // Takes a packet, serializes it then sends it out
+  // over the serial port.
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = serialize(buffer, packet, sizeof(TPacket));
+  writeSerial(buffer, len);
+}
